a7892b9b257fa563b3368b5d0128c9aa87f7f262
Scott Schwab <scott.schwab@gmail.com>
2019-06-26 21:35:59 -0500

    adding update

--- az2keyboard/__init__.py
+++ az2keyboard/__init__.py
@@ -4,15 +4,16 @@ from  azure.cosmos import cosmos_client
 import azure.cosmos.documents as documents
 import json
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)\
 
 
 def main(req: func.HttpRequest, 
         documentsin: func.DocumentList,
         documentsout: func.Out[func.Document]) -> func.HttpResponse:
-
+    
     body = list()
+    body_bag = {"messages": body}
     for doc in documentsin:
         d = dict()
         for keep in ['user_name', 'key', 'color', 'effect', 'text']:
@@ -20,8 +21,8 @@ def main(req: func.HttpRequest,
         body.append(d)
         logger.info(body)
 
-        # doc['read'] = 'T'
-        # documentsout.set(func.Document.from_dict(doc)
+        doc['read'] = 'T'
+        documentsout.set(func.Document.from_dict(doc))
         body_bag = { "messages" : body }
     return func.HttpResponse(
         body = json.dumps(body_bag), 
